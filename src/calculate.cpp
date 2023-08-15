e9bc03b358d196ef0f374dc692f471317e58e79d
EMEnderMan2581 <94626653+EMEnderMan2581@users.noreply.github.com>
2023-08-15 15:58:27 +0800

    Add files via upload

---
+++ src/calculate.cpp
@@ -0,0 +1,36 @@
+#include<stdexcept>
+
+#include"calculate.h"
+
+using namespace std;
+
+double calculate(double op1, char op, double op2)
+{
+	double result = 0.0;
+	switch (op)
+	{
+	case '+':
+		result = op1 + op2;
+		break;
+	case '-':
+		result = op1 - op2;
+		break;
+	case '*':
+		result = op1 * op2;
+		break;
+	case '/':
+		if (op2 == 0)
+		{
+			throw runtime_error("Error: Divided by zero");
+		}
+		else
+		{
+			result = op1 / op2;
+		}
+		break;
+	default:
+		throw runtime_error("Error: Unsupported operator");
+		break;
+	}
+	return result;
+}
