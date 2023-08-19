de038d44fe8b210a788fd424a802558cd5874094
EMEnderMan2581 <94626653+EMEnderMan2581@users.noreply.github.com>
2023-08-19 18:50:14 +0800

    Changed parameter names and fixed some bugs
    version 1.1

--- src/calculate.cpp
+++ src/calculate.cpp
@@ -1,35 +1,33 @@
-#include<stdexcept>
-
 #include"calculate.h"
 
 using namespace std;
 
-double calculate(double op1, char op, double op2)
+double calculate(double operand1, char op, double operand2)
 {
 	double result = 0.0;
 	switch (op)
 	{
 	case '+':
-		result = op1 + op2;
+		result = operand1 + operand2;
 		break;
 	case '-':
-		result = op1 - op2;
+		result = operand1 - operand2;
 		break;
-	case '*':
-		result = op1 * op2;
+	case 'x':
+		result = operand1 * operand2;
 		break;
 	case '/':
-		if (op2 == 0)
+		if (operand2 == 0)
 		{
-			throw runtime_error("Error: Divided by zero");
+			throw runtime_error("Divided by zero");
 		}
 		else
 		{
-			result = op1 / op2;
+			result = operand1 / operand2;
 		}
 		break;
 	default:
-		throw runtime_error("Error: Unsupported operator");
+		throw runtime_error("Unsupported operator");
 		break;
 	}
 	return result;
--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,62 @@
-#include<iostream>
-#include<cstring>
-#include<iomanip>
-#include<sstream>
-#include<math.h>
-
 #include"calculate.h"
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	if (argc < 4)
+	if (argc < 4 && (strcmp(argv[1], "-h") == 1 || strcmp(argv[1], "-h") == -1))
 	{
 		cout << "Error: Insufficient parameters" << endl;
 		cout << "Usage: ./Calculator <Operand1> <Operator> <Operand2>" << endl; //<DecimalPlaces>
+		//cout << "Available operators: +, -, x and /" << endl;
+		//cout << "Note: If you want to Multiply Operand1 by Operand2, please enter 'c'" << endl;
 		return 1;
 	}
 
-	else if (strcmp(argv[1],"-h") == 0)
+	else if (argc == 2 && strcmp(argv[1], "-h") == 0)
 	{
-		cout << "Calculator v1.0" << endl;
+		cout << "Calculator version 1.1" << endl;
 		cout << "Usage: ./Calculator <Operand1> <Operator> <Operand2>" << endl;
+		cout << "Available operators: +, -, x and /" << endl;
+		//cout << "Note: If you want to Multiply Operand1 by Operand2, please enter 'c'" << endl;
 		return 0;
 	}
 
 	else
 	{
-		double op1 = atof(argv[1]);
+		/*char op1 = argv[2][0];
+
+		switch (op1)
+		{
+		case '+':
+			op1 = '+';
+			break;
+		case '-':
+			op1 = '-';
+			break;
+		case 'c':
+			op1 = '*';
+			break;
+		case '/':
+			op1 = '/';
+			break;
+		default:
+			op1 = '+';
+			break;
+		}*/
+
+		double value1 = atof(argv[1]);
+		//char op = op1;
 		char op = argv[2][0];
-		double op2 = atof(argv[3]);
+		double value2 = atof(argv[3]);
+
+		//Test
+		//cout << value1 << endl << op << endl << op1 << endl << value2 << endl;
 		
 		double result;
 		try
 		{
-			result = calculate(op1, op, op2);
+			result = calculate(value1, op, value2);
 			cout << fixed;
 			if (floor(result) == result)
 			{
@@ -41,10 +64,10 @@ int main(int argc, char* argv[])
 			}
 			else
 			{
-				
-				stringstream ss;
-				ss << fixed << showpoint << setprecision(15) << result;
-				string output = ss.str();
+				//Decimal places
+				stringstream ss0;
+				ss0 << fixed << showpoint << setprecision(15) << result;
+				string output = ss0.str();
 				unsigned long long pos = output.find_last_not_of('0');
 				if (pos != std::string::npos) {
 					output.erase(pos + 1);
@@ -53,9 +76,10 @@ int main(int argc, char* argv[])
 			}
 		}
 
-		catch (const exception& e)
+		catch (const exception& error)
 		{
-			cout << "Error: " << e.what() << endl;
+			cout << "Error: " << error.what() << endl;
+			//cout << "Usage: ./Calculator <Operand1> <Operator> <Operand2>" << endl;
 		}
 		
 	}
