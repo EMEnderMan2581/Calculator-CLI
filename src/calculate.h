1677a14048aaf8c1959aafd86a7557e04e588279
EMEnderMan2581 <94626653+EMEnderMan2581@users.noreply.github.com>
2023-08-19 18:56:10 +0800

    Changed the location of #include

--- src/calculate.h
+++ src/calculate.h
@@ -1,3 +1,12 @@
 #pragma once
 
+//main.cpp
+#include<iostream>
+#include<cstring>
+#include<iomanip>
+#include<sstream>
+#include<math.h>
+
+#include<stdexcept> //calculate.cpp
+
 double calculate(double op1, char op, double op2);
